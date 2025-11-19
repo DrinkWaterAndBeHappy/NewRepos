66886342233c4ba29cc5c59bc1811c83de0bdc9f
DrinkWaterAndBeHappy <s4mur41.95@mail.ru>
2025-11-19 19:42:11 +0300

    System.out.println изменён на логирование

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -12,7 +12,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-
 //        UserService userService = new UserServiceImpl(new UserDaoJDBCImpl());
 //
 //        userService.createUsersTable();
